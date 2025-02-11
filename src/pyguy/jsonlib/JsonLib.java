dd1aa0fe25491c354c59d4d2aec0b2c8b11a1eac
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2025-02-11 23:57:29 +0100

    Initial commit

---
+++ src/pyguy/jsonlib/JsonLib.java
@@ -0,0 +1,155 @@
+package pyguy.jsonlib;
+
+import arc.Events;
+import arc.struct.ObjectMap;
+import arc.util.Log;
+import arc.util.serialization.*;
+import mindustry.Vars;
+import mindustry.core.ContentLoader;
+import mindustry.ctype.MappableContent;
+import mindustry.game.EventType;
+import mindustry.mod.*;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+// This class is the internal implementation of the library. You do not need to understand any of the code here to use the library.
+// The public API is in the JsonLibWrapper class.
+public class JsonLib extends Mod
+{
+    private static final HashMap<String, HashMap<String, JsonValue>> customJson = new HashMap<>();
+    private static final ArrayList<JsonValue> latestValues = new ArrayList<>();
+    private static boolean shouldProxy = false;
+    private static String modName = "";
+
+    public JsonLib()
+    {
+        try
+        {
+            Log.info("[CustomJsonLib] Retrieving custom JSON fields.");
+
+            // Get access to the global ContentParser instance
+            Field contentParserField = Mods.class.getDeclaredField("parser");
+            contentParserField.setAccessible(true);
+
+            ContentParser contentParser = (ContentParser) contentParserField.get(Vars.mods);
+
+            // Get access to the global ContentParser instance's Json instance
+            Field jsonParserField = ContentParser.class.getDeclaredField("parser");
+            jsonParserField.setAccessible(true);
+
+            Json jsonParser = (Json) jsonParserField.get(contentParser);
+
+            // Get the mod name
+            Field currentModField = ContentParser.class.getDeclaredField("currentMod");
+            currentModField.setAccessible(true);
+
+            // Replace the contentNameMap to retrieve the name of the content being parsed, if you find a better way to do this PLEASE let me know
+            Field contentNameMapField = ContentLoader.class.getDeclaredField("contentNameMap");
+            contentNameMapField.setAccessible(true);
+
+            @SuppressWarnings("unchecked")
+            ObjectMap<String, MappableContent>[] contentNameMap = (ObjectMap<String, MappableContent>[]) contentNameMapField.get(Vars.content);
+
+            @SuppressWarnings("unchecked")
+            ObjectMap<String, MappableContent>[] replacementContentNameMapArray = new ObjectMap[contentNameMap.length];
+            int i = 0;
+            for (ObjectMap<String, MappableContent> map : contentNameMap)
+            {
+                ProxyObjectMap replacementMap = new ProxyObjectMap();
+
+                for (String key : map.keys())
+                {
+                    replacementMap.put(key, map.get(key));
+                }
+
+                replacementContentNameMapArray[i] = replacementMap;
+                i++;
+            }
+
+            contentNameMapField.set(Vars.content, replacementContentNameMapArray);
+
+            // Replace the ContentParser json parser with a custom one that can read custom JSON fields before passing it on to the original parser
+            Json newParser = new Json()
+            {
+                @Override
+                public <T> T readValue(Class<T> type, Class elementType, JsonValue jsonData, Class keyType)
+                {
+                    if (jsonData.has("customJson"))
+                    {
+                        try
+                        {
+                            modName = ((Mods.LoadedMod) currentModField.get(contentParser)).name;
+                        } catch (IllegalAccessException e)
+                        {
+                            throw new RuntimeException(e);
+                        }
+
+                        latestValues.clear();
+                        shouldProxy = true;
+
+                        for (JsonValue value : jsonData.get("customJson").iterator())
+                        {
+                            latestValues.add(value);
+                        }
+                    }
+
+                    return jsonParser.readValue(type, elementType, jsonData, keyType);
+                }
+            };
+
+            jsonParserField.set(contentParser, newParser);
+        } catch (NoSuchFieldException | IllegalAccessException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static JsonValue Get(String internalContentName, String fieldName)
+    {
+        if (customJson.containsKey(internalContentName))
+        {
+            HashMap<String, JsonValue> map = customJson.get(internalContentName);
+            if (map != null) return map.get(fieldName);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void init()
+    {
+        Events.on(EventType.ClientLoadEvent.class, event -> {
+            for (String key : customJson.keySet())
+            {
+                Log.info("[CustomJsonLib] Custom JSON for key: " + key + " with values: " + customJson.get(key).toString());
+            }
+        });
+    }
+
+    private static class ProxyObjectMap extends ObjectMap<String, MappableContent>
+    {
+        @Override
+        public MappableContent get(String key)
+        {
+            if (shouldProxy)
+            {
+                String fullKey = modName + "-" + key;
+                shouldProxy = false;
+
+                if (!customJson.containsKey(fullKey))
+                {
+                    customJson.put(fullKey, new HashMap<>());
+                }
+
+                HashMap<String, JsonValue> hashMap = customJson.get(fullKey);
+                for (JsonValue value : latestValues)
+                {
+                    hashMap.put(value.name, value);
+                }
+            }
+
+            return super.get(key);
+        }
+    }
+}
---
+++ src/pyguy/jsonlib/JsonLibWrapper.java
@@ -0,0 +1,102 @@
+package pyguy.jsonlib;
+
+import arc.util.serialization.JsonValue;
+
+/**
+ * The public API to interface with the {@link JsonLib} library.
+ * <p/>
+ * All the methods in this class MUST be called within your mod's {@code init()} method or after.
+ * Failing to do so will result in unexpected behavior.
+ */
+public class JsonLibWrapper
+{
+    /**
+     * Get a {@link String} field from a custom JSON definition.
+     *
+     * @param internalContentName The internal name of the piece of content that implements the custom JSON definition.
+     * @param fieldName           The name of the custom JSON field.
+     * @return The value of the custom JSON field, or {@code null} if it does not exist or is not a {@link String}.
+     */
+    public static String GetStringField(String internalContentName, String fieldName)
+    {
+        JsonValue value = JsonLib.Get(internalContentName, fieldName);
+
+        if (value != null && value.isString())
+        {
+            return value.asString();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get an {@link Integer} field from a custom JSON definition.
+     *
+     * @param internalContentName The internal name of the piece of content that implements the custom JSON definition.
+     * @param fieldName           The name of the custom JSON field.
+     * @return The value of the custom JSON field, or {@code null} if it does not exist or is not an {@link Integer}.
+     */
+    public static Long GetLongField(String internalContentName, String fieldName)
+    {
+        JsonValue value = JsonLib.Get(internalContentName, fieldName);
+
+        if (value != null && value.isLong())
+        {
+            return value.asLong();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get a {@link Double} field from a custom JSON definition.
+     *
+     * @param internalContentName The internal name of the piece of content that implements the custom JSON definition.
+     * @param fieldName           The name of the custom JSON field.
+     * @return The value of the custom JSON field, or {@code null} if it does not exist or is not a {@link Double}.
+     */
+    public static Double GetDoubleField(String internalContentName, String fieldName)
+    {
+        JsonValue value = JsonLib.Get(internalContentName, fieldName);
+
+        if (value != null && value.isDouble())
+        {
+            return value.asDouble();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get a {@link Boolean} field from a custom JSON definition.
+     *
+     * @param internalContentName The internal name of the piece of content that implements the custom JSON definition.
+     * @param fieldName           The name of the custom JSON field.
+     * @return The value of the custom JSON field, or {@code null} if it does not exist or is not a {@link Boolean}.
+     */
+    public static Boolean GetBooleanField(String internalContentName, String fieldName)
+    {
+        JsonValue value = JsonLib.Get(internalContentName, fieldName);
+
+        if (value != null && value.isBoolean())
+        {
+            return value.asBoolean();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get a raw {@link JsonValue} field from a custom JSON definition.
+     * <p/>
+     * This must be used to get fields that are objects or arrays.
+     *
+     * @param internalContentName The internal name of the piece of content that implements the custom JSON definition.
+     * @param fieldName           The name of the custom JSON field.
+     * @return The value of the custom JSON field as a {@link JsonValue}, or {@code null} if it does not exist.
+     */
+    public static JsonValue GetRawField(String internalContentName, String fieldName)
+    {
+        return JsonLib.Get(internalContentName, fieldName);
+    }
+}
